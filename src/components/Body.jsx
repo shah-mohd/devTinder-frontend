33d82d4ffec07203594f30fad4b71d271718083d
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-29 20:33:59 +0530

    redireted login if not authenticated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,22 +1,28 @@
 import axios from 'axios'
 import Footer from './Footer'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { BASE_URL } from '../utils/constants'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addUser } from '../utils/userSlice'
 import { useEffect } from 'react'
 
 const Body = () => {
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector(store => store.user);
 
   const fetchUser = async () => {
+    if(userData) return;
     try{
         const res = await axios.get(BASE_URL + "/profile/view", {withCredentials: true});
         dispatch(addUser(res.data));
     } catch(err) {
-      console.log(err);
+      if(err.status === 400){
+        navigate("/login")
+      }
+      console.log(err);  
     }
   }
 
