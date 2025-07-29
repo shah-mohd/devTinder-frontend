abc873599242465d26c9b60216161f3fcae84d7e
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-29 14:45:37 +0530

    footer

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,3 +1,4 @@
+import Footer from './Footer'
 import NavBar from './NavBar'
 import { Outlet } from 'react-router-dom'
 
@@ -6,6 +7,7 @@ const Body = () => {
     <div>
         <NavBar />
         <Outlet />
+        <Footer />
     </div>
   )
 }
