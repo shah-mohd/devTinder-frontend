c9f7b32bd75975dc3e39b740d03b1f97d4cc1ed7
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-29 18:48:06 +0530

    useSelector in navbar - dynamic photo

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -8,14 +8,15 @@ const NavBar = () => {
     <div className="flex-1">
       <a className="btn btn-ghost text-xl">devTinder</a>
     </div>
-    <div className="flex gap-2">
-      {/* <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" /> */}
-      {user && <div className="dropdown dropdown-end mx-5">
+
+    {user && <div className="flex gap-2 items-center">
+      <div className="">Welcome, {user.firstName}</div>
+      <div className="dropdown dropdown-end mx-5">  
         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
           <div className="w-10 rounded-full">
             <img
               alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              src={user.photoUrl} />
           </div>
         </div>
         <ul
@@ -30,8 +31,8 @@ const NavBar = () => {
           <li><a>Settings</a></li>
           <li><a>Logout</a></li>
         </ul>
-      </div> }
-    </div>
+      </div>
+    </div>}
     </div>
   )
 }
