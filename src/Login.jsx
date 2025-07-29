b9885f9d2eb5f38475b88d56ef0be104bbd7c64b
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-29 16:53:48 +0530

    Login page + axios

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,55 @@
+import axios from "axios";
+import { useState } from "react"
+
 const Login = () => {
+
+  const [emailId, setEmailId] = useState("shah@gmail.com");
+  const [password, setPassword] = useState("Shah@123");
+
+  const handleLogin = async () => {
+    try{
+      const res = axios.post("http://localhost:7777/login",{
+        emailId,
+        password
+      },
+    {
+      withCredentials: true
+    });
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+
   return (
-    <div>Login</div>
+    <div className="flex justify-center my-10">
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <legend className="fieldset-legend">Login</legend>
+
+                <label className="label">Email</label>
+                <input 
+                type="email"
+                value={emailId}
+                className="input"
+                placeholder="Email"
+                onChange={(e) => setEmailId(e.target.value)}
+                />
+
+                <label className="label">Password</label>
+                <input
+                type="password"
+                value={password}
+                className="input"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button 
+                className="btn btn-neutral mt-4"
+                onClick={handleLogin}
+                >Login</button>
+            </fieldset>
+    </div>
   )
 }
 
