28b3c647da290f6f8fd5c44de97ddb1c244f9472
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-30 18:39:15 +0530

    connection page re build

--- src/components/UserConnectionProfile.jsx
+++ src/components/UserConnectionProfile.jsx
@@ -1,18 +1,17 @@
 const UserConnectionProfile = ({user}) => {
     const {firstName, lastName, photoUrl, age, gender, about} = user;
   return (
-        <div className="card bg-base-100 w-60 shadow-sm">
-        <figure>
+        <div className="flex border-2 rounded-sm p-5">
+        <div className="w-32">
             <img
+            className=""
             src={photoUrl}
             alt="feedUser" />
-        </figure>
-        <div className="card-body">
+        </div>
+        <div className="">
             <h2 className="card-title">{firstName + " " + lastName}</h2>
             {about && <p>{about}</p>}
-            {age && gender && <p>{age + ", " + gender}</p>}
-            <div className="card-actions justify-center flex-nowrap mx-4">
-        </div>
+            {age && gender && <p>{age + ", " + gender}</p>} 
         </div>
     </div>
   )
